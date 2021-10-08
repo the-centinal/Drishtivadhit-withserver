b50088a468a557edc9c6f5713d4d452b8a538793
priyanshu-rajput-01 <priyanshurajput0071109@gmail.com>
2021-10-08 14:48:47 +0530

    Update document schema

--- models/document.js
+++ models/document.js
@@ -1,9 +1,4 @@
 const mongoose = require("mongoose");
-const path = require('path');
-const file_path = path.join('/public/uploads');
-const multer = require('multer');
-const {GridFsStorage} = require("multer-gridfs-storage");
-const util = require("util");
 const docSchema = new mongoose.Schema({
     // userFiles : {
     //     type:Array,
@@ -27,26 +22,6 @@ const docSchema = new mongoose.Schema({
     }
       
 },  {timestamps: true});
-
-let filestorage = multer.diskStorage({destination:(req,res,cb)=>{
-    cb(null,path.join(__dirname,'..',file_path));
-},
-filename:(req,file,cb)=>{
-    cb(null,Date.now()+"--"+file.fieldname);
-},
-});
-
-var storage = new GridFsStorage({
-    url: process.env.DB,
-    options: { useNewUrlParser: true, useUnifiedTopology: true },
-    filename:(req,file,cb)=>{
-        cb(null,Date.now()+"--"+file.fieldname);
-    }
-  });
-
-docSchema.statics.uploadedFile = multer({storage:storage}).single('userFiles');
-docSchema.statics.filepath = file_path;
-// docSchema.statics.uploadedFile = multer({storage:storage}).single('userFiles');
 
 const Documents = new mongoose.model("Documents", docSchema);
 
